ee7130de696f6614f56fff7cb43130fbcbc1e23c
Sarthak Sethi <sarthaksethigithub@gmail.com>
2020-05-08 02:09:46 +0530

    Added Text Recognition

--- app/src/main/java/com/example/imagelabellingml/ImageDetect.java
+++ app/src/main/java/com/example/imagelabellingml/ImageDetect.java
@@ -22,6 +22,8 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,7 +37,7 @@ public class ImageDetect extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_image_detect);
         imgbtn = findViewById(R.id.image);
         displayname = findViewById(R.id.displayobjname);
         displayconfidence = findViewById(R.id.displayobjpercentage);
@@ -60,6 +62,7 @@ public class ImageDetect extends AppCompatActivity {
             try {
                 image = FirebaseVisionImage.fromFilePath(getApplicationContext(), data.getData());
                 FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler();
+                FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
                 labeler.processImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
                     @Override
                     public void onSuccess(List<FirebaseVisionImageLabel> labels) {
@@ -80,6 +83,21 @@ public class ImageDetect extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(),"Task Failed",Toast.LENGTH_LONG).show();
                             }
                         });
+
+                textRecognizer.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText result) {
+                                displaytextfromimage.setText(result.getText());
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(),"Task Failed",Toast.LENGTH_LONG).show();
+                                    }
+                                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
